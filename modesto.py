bb22bf86ff7f5aaef037b52cddcc7f01f4213578
Vekin803 <KiNd138877>
2018-09-25 12:09:17 +0200

    modesto.py

---
+++ modesto.py
@@ -0,0 +1,25 @@
+
+import fdb
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
+app = Flask('__name__')
+api = Api(app)
+
+class Student(Resource):
+    def get(self, name):
+        con = fdb.connect( dsn='C:\\Users\modes\Documents\Software MSM\Servitec CS\STECCS_MGV.FDB',
+                           user='sysdba',
+                           password='masterkey',
+                           charset='UTF8')
+        cur = con.cursor()
+        result = cur.execute('select * from FORMAS_DE_PAGO')
+        items = []
+        for row in result:
+            items.append({'Forma Pago': row[0], 'Emitir Recibo': row[1], 'Remesar': row[2]})
+        con.close()
+        return {'Items': items}
+
+api.add_resource(Student, '/student/<string:name>')
+
+app.run(port=5000)
