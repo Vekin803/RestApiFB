dcbd2e74dd2a3204670e6f37d15322164402f130
Vekin803 <KiNd138877>
2018-09-25 14:02:49 +0200

    more changes

--- app.py
+++ app.py
@@ -10,5 +10,19 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
+@app.route('/signUp',methods=['POST'])
+def signUp():
+
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
 if __name__ == "__main__":
     app.run()
