7fad2de16f5529dec403f80715077d7541e524e1
Vekin803 <KiNd138877>
2018-10-01 15:56:05 +0200

    changes

--- resources/user.py
+++ resources/user.py
@@ -6,3 +6,12 @@ from connect_fdb import Consulta
 class UserName(Resource):
     def get(self):
         return Consulta("select Nombre, Telefono1, Email from CLIENTES")
+
+    def post(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
