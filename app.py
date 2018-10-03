49f17ecda91f2c024e9b3d7196801583b23d55cd
Vekin803 <KiNd138877>
2018-10-03 11:33:32 +0200

    changes

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import fdb
-from flask import Flask, jsonify, json
+from flask import Flask, jsonify, json, render_template
 from flask_restful import Api
 from flask_cors import CORS
 from flask_jwt import JWT
 
 from resources.formaspago import FormasPago
-from resources.users import UserName
+from resources.users import UserName, Login
 from security import authenticate, identity
-
 
 app = Flask('__name__')
 app.secret_key = 'mi_clave_secreta'
@@ -16,7 +15,12 @@ CORS(app)
 
 jwt = JWT(app, authenticate, identity)
 
+@app.route('/')
+def main():
+    return render_template('login.html')
 
+
+api.add_resource(Login, '/login')
 api.add_resource(FormasPago, '/formaspago/')
 api.add_resource(UserName, '/user/<string:username>')
 
--- resources/users.py
+++ resources/users.py
@@ -1,7 +1,20 @@
 
+from flask import request
 from flask_restful import Resource
 
 from connect_fdb import Consulta
+
+class Login(Resource):
+    def post(self):
+        username = request.form['username']
+        password = request.form['password']
+        user = Consulta("SELECT Nombre, Telefono1, Email FROM CLIENTES WHERE NOMBRE = '{}' AND CLAVE_WP = '{}'".format(username, password))
+        if user:
+            return { 'user': user }
+        else:
+            return {'message': 'User no existe'}
+
+
 
 class UserName(Resource):
     def get(self, username):
