33c21db990ac3229c780f1acf8c15a7e245f57e6
MSMSoftware <modesto@msm-software.com>
2018-10-09 12:45:20 +0200

    Con tokens

--- carpeta_modesto/STR.py
+++ carpeta_modesto/STR.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask_restful import Api
-from
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-api = Api(app) 
+api = Api(app)
 
 import Resources
-app.config['SECRET_KEY'] = 'masterkey'
+app.config['JWT_SECRET_KEY'] = 'masterkey'
+#app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
+#
+#@jwt.token_in_blacklist_loader
+#def check_if_token_in_blacklist(decrypted_token):
+#    jti = decrypted_token['jti']
+#    return models.RevokedTokenModel.is_jti_blacklisted(jti)
+#
+
+jwt = JWTManager(app)
 
 @app.route('/')
 def hola():
--- carpeta_modesto/dm.py
+++ carpeta_modesto/dm.py
@@ -2,8 +2,8 @@ import fdb
 import json
 
 def ConsultaSQL(sql):
-    con = fdb.connect(host="127.0.0.1", database="C:\PRUEBA.FDB", user="sysdba", password="masterkey")
+    con = fdb.connect(host="127.0.0.1", database="C:\STECCS.FDB", user="sysdba", password="masterkey")
     cur = con.cursor()
     rows = cur.execute(sql)
-    items = [dict(zip([key[0] for key in cur.description], row)) for row in rows] 
+    items = [dict(zip([key[0] for key in cur.description], row)) for row in rows]
     return items
